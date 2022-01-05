89947301717ccedc5ee1410b1bae8922fef51b49
Cihat Eminsoy <cihateminsoy@gmail.com>
2022-01-05 21:03:09 +0800

    message2

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -1,6 +1,7 @@
 public class HelloTest {
     public static void main(String[] args) {
         System.out.println("hello world2");
+        System.out.println("this is practise");
 
         //this is just a message
     }
